51085e8823c53d4a2ebebe5d4f33e967e38e7f20
Yvens Pinto <pinto.yvens@ibm.com>
2025-11-13 15:41:42 +0530

    add multi touch support

--- scripts/letter_hunt_controls.gd
+++ scripts/letter_hunt_controls.gd
@@ -1,27 +1,23 @@
 extends CanvasLayer
 
+func _on_touch_screen_left_button_pressed() -> void:
+	Input.action_press("left")
+
+func _on_touch_screen_left_button_released() -> void:
+	Input.action_release("left")
 
 
-func _handle_touch_event(event: InputEvent, action) -> void:
-	if event is InputEventMouseButton:
-		# Check if it's a left button press (standard for clicks/touches)
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.is_pressed():
-				Input.action_press(action)
-				# Add logic for when the element is pressed down
-			elif event.is_released():
-				# Add logic for when the press/touch ends
-				Input.action_release(action)
+func _on_touch_screen_right_button_pressed() -> void:
+	Input.action_press("right")
 
 
-func _on_up_button_gui_input(event: InputEvent) -> void:
-	# This block handles both mouse clicks AND touch presses
-	_handle_touch_event(event, "jump")
+func _on_touch_screen_right_button_released() -> void:
+	Input.action_release("right")
 
 
-func _on_left_button_gui_input(event: InputEvent) -> void:
-	_handle_touch_event(event, "left")
+func _on_touch_screen_up_button_pressed() -> void:
+	Input.action_press("jump")
 
 
-func _on_right_button_gui_input(event: InputEvent) -> void:
-	_handle_touch_event(event,"right")
+func _on_touch_screen_up_button_released() -> void:
+	Input.action_release("jump")
